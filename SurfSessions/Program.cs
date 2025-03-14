caff7753aa1296e7dcaad1d0940408dbd1044b8e
GDelaunay <goulwendelaunay@gmail.com>
2025-03-14 15:20:46 +0100

    build: Ajout et config d'ElectronNET - Configuration du bridge d'Electron permettant de lancer l'application web en mode fenêtré autonome - Ajout d'un splashscreen et modif du favicon.ico

--- SurfSessions/Program.cs
+++ SurfSessions/Program.cs
@@ -1,12 +1,19 @@
 using SurfSessions;
+using ElectronNET.API;
+using SocketIOClient;
 
+// Connexion EF Core et vérification que les tables existent
 using AppDbContext context = new AppDbContext();
 context.Database.EnsureCreated();
 
+// Création du webApp builder
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseElectron(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+// Is optional, but you can use the Electron.NET API-Classes directly with DI (relevant if you want more encoupled code)
+builder.Services.AddElectron();
 
 var app = builder.Build();
 
@@ -30,4 +37,23 @@ app.MapControllerRoute(
         pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
-app.Run();
+
+await app.StartAsync();
+
+// Open the Electron-Window here
+try
+{
+    await Electron.WindowManager.CreateWindowAsync();
+
+}
+catch (ConnectionException e)
+{
+    Console.BackgroundColor = ConsoleColor.White;
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.Write("warn");
+    Console.ResetColor();
+    if(e.StackTrace != null){Console.Write(": " + e.StackTrace[6..].Split("(")[0] + "\n");}
+    Console.WriteLine("      Failed to establish connection to Electron. \n");
+}
+
+app.WaitForShutdown();
